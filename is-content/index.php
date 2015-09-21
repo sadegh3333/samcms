8348eb2e85278cb3af71ae70a08653612c00dee4
sadegh3333 <sadegh3333@gmail.com>
2015-09-21 23:16:55 +0430

    Update index.php
    add comments

--- is-content/index.php
+++ is-content/index.php
@@ -13,16 +13,19 @@ require ('../is-include/jdf.php');
 require ('../is-include/function.php');
 require ('../is-include/template-class.php');
 
-
+// make a new object from template class
 $tpl = new Template();
 $tpl->load_file('theme/index.htm');
 
+// get header 
 include('header.php');
 
+// get page 
 include('page.php');
 
 // Create template for show
 $tpl->print_template();
 
+// close database connection
 $dbc->close();
 ?>
