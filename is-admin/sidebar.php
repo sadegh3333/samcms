68520fac0aca9544c549ff601631f96d8bd350c1
sadegh <sadegh333@gmail.com>
2015-12-11 19:48:36 +0330

    add icon for sidebar items in admin panel

--- is-admin/sidebar.php
+++ is-admin/sidebar.php
@@ -4,7 +4,7 @@
  * @author Sadegh Mahdilou
  * @copyright 2015
  * @since January 2013 - 2015 December 
- * @version 0.6.0 Beta
+ * @version 0.6.1 Beta
  */
 
 
@@ -13,18 +13,18 @@
 <div id="sidebar-wrapper">
 	<ul class="sidebar-nav">
 		<li>
-			<a href="<?php echo $Root.'/is-admin'; ?>">Dashboard</a>
+			<a href="<?php echo $Root.'/is-admin'; ?>"><i class="fa fa-tachometer"></i>Dashboard</a>
 		</li>
 		<li>
-			<a href="<?php echo $Root.'/is-admin/showallpost.php'; ?>">Posts</a>
+			<a href="<?php echo $Root.'/is-admin/showallpost.php'; ?>"><i class="fa fa-archive"></i> Posts</a>
 			<ul>
 				<li>
-					<a href="<?php echo $Root.'/is-admin/post.php'; ?>">Add post</a>
+					<a href="<?php echo $Root.'/is-admin/post.php'; ?>"><i class="fa fa-plus-square"></i>Add post</a>
 				</li>
 			</ul>
 		</li>
 		<li>
-			<a href="<?php echo $Root.'/is-admin/cat.php'; ?>">Category</a>
+			<a href="<?php echo $Root.'/is-admin/cat.php'; ?>"><i class="fa fa-briefcase"></i>Category</a>
 		</li>
 	</ul>
 </div>
