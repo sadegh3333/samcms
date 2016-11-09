ab606d485725a13c0c9cf5006e91a2be7803d774
sadegh3333 <sadegh3333@gmail.com>
2016-11-09 23:28:35 +0330

    add new class for template part

--- is-include/plugin.class.php
+++ is-include/plugin.class.php
@@ -1,5 +1,14 @@
 <?php 
 
+/**
+ * @author Sadegh Mahdilou
+ * @copyright 2016
+ * @since  Nov 2016
+ * @since version 0.4.0
+ * @version 0.2.0 Beta
+ */
+
+
 
 /*
 *	This is Plugin API
---
+++ is-include/template-class.php
@@ -0,0 +1,30 @@
+<?php 
+
+/**
+ * @author Sadegh Mahdilou
+ * @copyright 2016
+ * @since  Nov 2016
+ * @since version 0.4.0
+ * @version 0.2.0 Beta
+ */
+
+
+/*
+*
+*	This is template class
+*	Everything about templale functions and anything create here.
+*	created new object when run theme
+*/
+
+
+
+
+class template 
+{
+
+	function __construct(){
+		
+	}
+
+
+}
